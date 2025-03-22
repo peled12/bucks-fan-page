01512213ca19c84f5d1725bba278b55b26f17ae1
Peled12 <korenpeled08@gmail.com>
2025-03-22 12:25:56 +0200

    applied third-party api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -343,7 +343,7 @@ function App() {
   useEffect(() => {
     if (isScheduleError) setNextBucksGame("error") // for display msg
 
-    if (!schedule) return // make sure schedule is fethced
+    if (!schedule.length) return // make sure schedule is fethced
 
     const currentDate = getCurrentDate()
 
@@ -361,6 +361,8 @@ function App() {
 
     // get the game box score if its valid:
     if (nextGame) {
+      console.log()
+
       const boxScore = nextGame
       setNextBucksGame(boxScore)
     } else setNextBucksGame("no games left")
@@ -417,6 +419,8 @@ function App() {
 
     // schedule:
     async function getScheduleData() {
+      console.log("fetching schedule")
+
       try {
         const res = await axios.request(scheduleOptions)
 
@@ -449,11 +453,19 @@ function App() {
     const today = new Date().toLocaleDateString() // get the date
     const lastRun = localStorage.getItem("lastRunDate")
 
+    console.log(today, lastRun)
+
+    console.log(lastRun !== today, !lastRun)
+
     // last run was not today or its the first run
     if (lastRun !== today || !lastRun) {
       // this runs max once between midnights
 
+      console.log("hi")
+
       const isBetweenOct15toJune = isDateBetweenOct15ToJune()
+
+      console.log(isBetweenOct15toJune)
 
       localStorage.removeItem("lastBucksGame")
       setLastBucksGame(null)
@@ -502,17 +514,13 @@ function App() {
     }
 
     function isDateBetweenOct15ToJune() {
-      const now = new Date()
-      const currentYear = now.getFullYear()
+      const today = new Date()
+      const year = today.getFullYear()
 
-      const oct15 = new Date(currentYear, 9, 15)
-      const dec31 = new Date(currentYear, 11, 31)
-      const jan1 = new Date(currentYear + 1, 0, 1)
-      const apr30 = new Date(currentYear + 1, 5, 30)
+      const oct15 = new Date(year, 9, 15)
+      const jun30 = new Date(year, 5, 30)
 
-      // check if the current date is between October 15th and December 31st (current year)
-      // or between January 1st and April 30th (next year)
-      return (now >= oct15 && now <= dec31) || (now >= jan1 && now <= apr30)
+      return today >= oct15 || today <= jun30
     }
   }, [])
 
